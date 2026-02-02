a4a1c1991e5524f109abdf69cbf6583d1d2eed4b
enter-pro <github@enter.pro>
2026-02-02 14:48:02 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,8 +2,8 @@ import { PasswordGenerator } from '@/components/PasswordGenerator';
 
 const Index = () => {
   return (
-    //<div className="min-h-screen w-full bg-gradient-to-br from-[#667eea] to-[#764ba2] flex items-center justify-center p-5 md:p-8">
-      <div className="min-h-screen w-full bg-gradient-to-br from-[#0093E9] to-[#80D0C7] flex items-center justify-center p-5 md:p-8">
+    <div className="min-h-screen w-full bg-gradient-to-br from-[#667eea] to-[#764ba2] flex items-center justify-center p-5 md:p-8">
+      //<div className="min-h-screen w-full bg-gradient-to-br from-[#0093E9] to-[#80D0C7] flex items-center justify-center p-5 md:p-8">
       <PasswordGenerator />
     </div>
   );
